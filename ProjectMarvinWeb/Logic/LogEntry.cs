955a149c8f604e6c4f9d0f1b49716e605fc2b9c1
Lars Siden <larssiden@gmail.com>
2024-08-23 22:47:50 +0200

    Update

--- ProjectMarvinWeb/Logic/LogEntry.cs
+++ ProjectMarvinWeb/Logic/LogEntry.cs
@@ -4,43 +4,44 @@ using System.Web;
 
 public class LogEntry
 {
-	public int Id { get; set; }
-	public string? Message { get; set; }
-	public DateTime? LogDate { get; set; }
-	public string LogDateSE => LogDate?.ToString("yyyy-MM-dd HH:mm:ss") ?? "";
-	public string? IPAdress { get; set; }
-	public string? Sender { get; set; }
-	public string LogType { get; set; }
+  public int Id { get; set; }
+  public string? Message { get; set; }
+  public DateTime? LogDate { get; set; }
+  public string LogDateSE => LogDate?.ToString("yyyy-MM-dd HH:mm:ss") ?? "";
+  public string? IPAdress { get; set; }
+  public string? Sender { get; set; }
+  public string LogType { get; set; }
 
-	public LogEntry(string msg)
-	{
-		Message = msg;
-	}
-	public LogEntry()
-	{
+  public LogEntry(string msg)
+  {
+    Message = msg;
+  }
+  public LogEntry()
+  {
 
-	}
-	public static LogEntry Load(string JSONString)
-	{
-		try
-		{
+  }
+  public static LogEntry Load(string JSONString)
+  {
+    try
+    {
 
-			LogEntry logEntry = JsonSerializer.Deserialize<LogEntry>(JSONString,
-				new JsonSerializerOptions
-				{
-					PropertyNameCaseInsensitive = true,
-					IncludeFields = true
-				}
-				) ?? new LogEntry(JSONString);
+      LogEntry logEntry = JsonSerializer.Deserialize<LogEntry>(JSONString,
+        new JsonSerializerOptions
+        {
+          PropertyNameCaseInsensitive = true,
+          IncludeFields = true
+        }
+        ) ?? new LogEntry(JSONString);
 
-			byte[] bytes = Encoding.GetEncoding("iso-8859-1").GetBytes(logEntry.Message);
-			string decodedString = HttpUtility.UrlDecode(logEntry.Message, Encoding.GetEncoding("iso-8859-1"));
-			logEntry.Message = decodedString;
-			return logEntry;
-		}
-		catch
-		{
-			return new LogEntry(JSONString);
-		}
-	}
+      string decodedString = HttpUtility.UrlDecode(logEntry.Message ?? "", Encoding.GetEncoding("utf-8"));
+      logEntry.Message = decodedString;
+      string decodedStringSender = HttpUtility.UrlDecode(logEntry.Sender ?? "", Encoding.GetEncoding("utf-8"));
+      logEntry.Sender = decodedStringSender;
+      return logEntry;
+    }
+    catch
+    {
+      return new LogEntry(JSONString);
+    }
+  }
 }
